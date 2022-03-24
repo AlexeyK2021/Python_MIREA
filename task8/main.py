f8c308f8692999768f2ebf13bfd80f873cf9533c
Alexey <kalashnickov.alexey2015@yandex.ru>
2022-03-24 17:03:50 +0300

    Сдана 8. Начата 7

--- task8/main.py
+++ task8/main.py
@@ -1,13 +1,15 @@
 def main(text):
     result = {}
-    data = text.split('; ')
+    data = text.split(';')
     for str in data:
-        result[str[str.find('->@"') + 4:str.find('" %')]] = str[str.find('local ') + 6:str.find(' ->')]
+        if str.find(".end") == -1:
+            result[str[str.find('@"') + 2:str.rfind('"')]] = \
+                str[str.find('local ') + 6:str.find('->')-1]
     return result
 
 
 if __name__ == "__main__":
     print(main(
-        '.do <% local beesla_355 ->@"bege_107" %>; <% local orusso ->@"onbein"%>; <% local abivear_778 ->@"anrala" '
-        '%>;<% local mama -> @"ledi"%>;.end').items(),
-          sep="\n")
+        '.do<% local geanqu -> @"sotius_407" %>;<% local atarqu_230 ->@"vetece" %>;<% local quma -> @"rigema_118"%>; .end').items())
+    print(main(
+        '.do <% local beesla_355 ->@"bege_107" %>; <% local orusso ->@"onbein" %>; <% local abivear_778 ->@"anrala" %>;<% local mama -> @"ledi" %>;.end').items())
