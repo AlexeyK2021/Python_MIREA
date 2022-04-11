393647414eb2de7fe13958a542d0de15009f1b60
unknown <ivcbibl212@VLAN-25-blb-003.rd.mirea.ru>
2022-04-11 12:02:37 +0300

    пр 5_1,2

---
+++ pract5_1/test.py
@@ -0,0 +1,17 @@
+from main import calculate as calc
+
+
+def test_1():
+    assert calc("1 + 3") == 4
+
+
+def test_2():
+    assert calc("4 * 6") == 24
+
+
+def test_3():
+    assert calc("5 /0") == 'Деление на ноль'
+
+
+if __name__ == "__main__":
+    pass
